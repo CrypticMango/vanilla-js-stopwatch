604dd5f0a6caf12631fcaea5580ae80846393948
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-08-24 13:59:59 -0600

    declared variables

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-//alert('hello!')
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const resetButton = document.getElementById("reset");
+const minute = document.getElementById("minute");
+const seconds = document.getElementById("seconds");
+const milliseconds = document.getElementById("milliseconds");
